72125b126f264a9891b3aed2be366eecae7a6785
Joyce Ren <joyce.y.ren@gmail.com>
2017-10-30 17:53:22 -0400

    added length validation

--- server/db/models/review.js
+++ server/db/models/review.js
@@ -1,20 +1,24 @@
-const crypto = require('crypto')
-const Sequelize = require('sequelize')
-const db = require('../db')
+const Sequelize = require('sequelize');
+const db = require('../db');
 
 const Review = db.define('review', {
   stars: {
     type: Sequelize.INTEGER,
+    allowNull: false,
     validate: {
-      notNull: true,
-      max: 5,
+      max: 5
     },
   },
   title: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
   },
   text: {
-    type: Sequelize.TEXT
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      len: [0, 100]
+    }
   },
 })
 
