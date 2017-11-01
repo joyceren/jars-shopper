809f83202f6e3efadc3987a318f1b2bae851be1a
Joyce Ren <joyce.y.ren@gmail.com>
2017-11-01 15:03:00 -0400

    moved OrderProducts model to separate file and fixed update routes for api/users and api/categories

--- server/api/users.js
+++ server/api/users.js
@@ -26,14 +26,10 @@ router.get('/:id', (req, res, next) => {
 })
 
 router.put('/:id', (req, res, next) => {
-  User.update(req.body, {
-    where: {
-      id: req.params.id
-    },
-    returning: true
-  })
-   .then(user => res.status(200).json(user))
-   .catch(next)
+  User.findById(req.params.id)
+    .then(user => user.update(req.body)
+    .then(user => res.status(200).json(user))
+    .catch(next)
 })
 
 router.post('/', (req, res, next) => {
@@ -56,4 +52,3 @@ router.delete('/:id', (req, res, next) => {
     .then(() => res.status(204).end())
     .catch(next);
 })
-
