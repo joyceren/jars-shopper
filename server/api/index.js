6aa2d447085158778d44a96e156945548da8ab5b
Renee Sajedian <rsajedian@gmail.com>
2017-11-03 11:21:03 -0400

    made cartMaker not break all the routes

--- server/api/index.js
+++ server/api/index.js
@@ -2,25 +2,28 @@ const router = require('express').Router();
 const { Order } = require('../db/models');
 module.exports = router;
 
-// function cartMaker(req, res, next) {
-//   if (req.cart) return next();
-//   const cartId = req.session.cartId;
-//   if (cartId) {
-//     return Order.findById(cartId)
-//       .then(cart => {
-//         req.cart = cart
-//         next()
-//       })
-//   }
-//   else {
-//     Order.create()
-//     .then(cart => {
-//       req.session.cartId = cart.id
-//     })
-//   }
-// }
-// router.use(cartMaker)
+function cartMaker(req, res, next) {
+  if (req.cart) return next();
+  const cartId = req.session.cartId;
+  if (cartId) {
+    return Order.findById(cartId)
+      .then(cart => {
+        req.cart = cart
+        next()
+      })
+      .catch(next)
+  }
+  else {
+    Order.create()
+    .then(cart => {
+      req.session.cartId = cart.id
+      res.send(cart);
+    })
+    .catch(next)
+  }
+}
 
+router.use(cartMaker)
 router.use('/users', require('./users'))
 router.use('/products', require('./products'));
 router.use('/orders', require('./orders'));
