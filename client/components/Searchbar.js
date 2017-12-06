be4f8c09c570d1e728f292cab8c639df6a6165cb
Arianna Lanz <ariannalanz@gmail.com>
2017-12-06 15:44:44 -0500

    can render searched product

--- client/components/Searchbar.js
+++ client/components/Searchbar.js
@@ -1,90 +1,99 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import AutoComplete from 'material-ui/AutoComplete';
 import { connect } from 'react-redux';
 import {fetchProducts, fetchProduct} from '../store';
 import SearchResults from './SearchResults';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-// import Category from './Category';
 
 
 export class Searchbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      productNames: [],
-      query: ''
-    }
-    this.getProductNames = this.getProductNames.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-    this.handleNewRequest = this.handleNewRequest.bind(this);
-    this.clearQuery = this.clearQuery.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+constructor(props) {
+  super(props);
+  this.state = {
+    productNames: [],
+    query: ''
   }
-
-  handleInput(query) {
-    this.setState({
-      query: query
-    });
-  }
-
-  handleNewRequest() {
-    this.setState({
-      query: this.state.query
-    });
-  }
-
-  clearQuery() {
-    this.setState({
-      query: ''
-    })
-  }
-
-  handleSubmit(event) {
-
-  }
-
-  getProductNames(products) {
-    let productNames = [];
-		for (let i = 0; i < products.length; i++) {
-			productNames.push(products[i].title);
-		}
-		return productNames;
-  }
-
-
-  render() {
-    const productNames = this.getProductNames(this.props.products)
-
-    const filteredProducts = this.props.products.filter(
-              product => product.title.toLowerCase().match(this.state.query.toLowerCase()));
-    return (
-      <div className="container">
-        <form onSubmit={ this.handleSubmit }>
-          <AutoComplete
-            hintText="Type in a product name here"
-            filter={ AutoComplete.fuzzyFilter }
-            dataSource={ productNames }
-            floatingLabelText="Search"
-            searchText={this.state.query}
-            onUpdateInput={this.handleInput}
-            onNewRequest={this.handleNewRequest}
-            fullWidth={ true }
-          />
-        </form>
-
-        {/* <Category /> */}
-        <br />
-        {/* <SearchResults filteredProducts={filteredProducts} /> */}
-      </div>
-    )
-  }
+  this.getProductNames = this.getProductNames.bind(this);
+  this.handleInput = this.handleInput.bind(this);
+  this.handleNewRequest = this.handleNewRequest.bind(this);
+  this.clearQuery = this.clearQuery.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
 }
 
-const mapStateToProps = function (state) {
-  return {
-    products: state.products,
+handleInput(query) {
+  this.setState({
+    query: query
+  });
+}
+
+handleNewRequest() {
+  this.setState({
+    query: this.state.query
+  });
+}
+
+clearQuery() {
+  this.setState({
+    query: ''
+  })
+}
+
+handleSubmit(event) {
+  event.preventDefault();
+  let products = this.props.products;
+
+  for (let i = 0; i < products.length; i++) {
+    if (this.state.query === products[i].title) {
+      this.context.router.history.push(`/products/${products[i].id}`);
+    }
   }
+  }
+
+getProductNames(products) {
+  let productNames = [];
+  for (let i = 0; i < products.length; i++) {
+    productNames.push(products[i].title);
+  }
+  return productNames;
+}
+
+
+render() {
+  const productNames = this.getProductNames(this.props.products)
+
+  const filteredProducts = this.props.products.filter(
+            product => product.title.toLowerCase().match(this.state.query.toLowerCase()));
+  return (
+    <div className="container">
+      <form onSubmit={ this.handleSubmit }>
+        <AutoComplete
+          hintText="Type in a product name here"
+          filter={ AutoComplete.fuzzyFilter }
+          dataSource={ productNames }
+          floatingLabelText="Search"
+          searchText={this.state.query}
+          onUpdateInput={this.handleInput}
+          onNewRequest={this.handleNewRequest}
+          fullWidth={ true }
+        />
+      </form>
+
+      {/* <Category /> */}
+      <br />
+      {/* <SearchResults filteredProducts={filteredProducts} /> */}
+    </div>
+  )
+}
+}
+
+Searchbar.contextTypes = {
+  router: PropTypes.object.isRequired
+};
+
+const mapStateToProps = function (state) {
+return {
+  products: state.products,
+}
 }
 
 // const mapDispatchToProps = function (dispatch) {
